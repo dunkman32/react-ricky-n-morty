8540b99b1d4c949c1ab630a2e66572b8d645402b
dunkman32 <dunkman032@gmail.com>
2020-02-16 01:51:07 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+
+import Episodes from './views/episodes';
+import Episode from './views/episode';
+import Notifications from './components/notifications';
+import PageNotFound from './views/page-not-found';
+
+import './App.css';
+
+const App = () => {
+	return (
+		<div className="App">
+			<Switch>
+				<Route exact path="/" component={Episodes}/>
+				<Route path="/episode/:id" component={Episode}/>
+				<Route component={PageNotFound}/>
+			</Switch>
+			<Notifications/>
+		</div>
+	);
+};
+
+export default App;
