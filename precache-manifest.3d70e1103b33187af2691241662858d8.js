aad8d5335e37d06e49d8197e52b0e4ad7c8c421a
nozza <nozza@localhost.localdomain>
2021-02-17 17:27:43 +0400

    Updates

---
+++ precache-manifest.3d70e1103b33187af2691241662858d8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6e5120676a04051b8d67a3305b5ec7c",
+    "url": "/react-ricky-n-morty/index.html"
+  },
+  {
+    "revision": "ed879a89cd5067e82a38",
+    "url": "/react-ricky-n-morty/static/css/2.caa94e78.chunk.css"
+  },
+  {
+    "revision": "b71948f75a230fda5762",
+    "url": "/react-ricky-n-morty/static/css/main.b91811cf.chunk.css"
+  },
+  {
+    "revision": "ed879a89cd5067e82a38",
+    "url": "/react-ricky-n-morty/static/js/2.23e0de8d.chunk.js"
+  },
+  {
+    "revision": "8d5ff9c1abe7ee712ff89802c9c33cc5",
+    "url": "/react-ricky-n-morty/static/js/2.23e0de8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b71948f75a230fda5762",
+    "url": "/react-ricky-n-morty/static/js/main.0d3fa157.chunk.js"
+  },
+  {
+    "revision": "55794b709ad0075dc9c5",
+    "url": "/react-ricky-n-morty/static/js/runtime-main.2129f14a.js"
+  },
+  {
+    "revision": "cb8495bfd8a20954e42a16c4c77c7927",
+    "url": "/react-ricky-n-morty/static/media/back.cb8495bf.jpg"
+  },
+  {
+    "revision": "b50f527235a697e0bfc9f4885da5b210",
+    "url": "/react-ricky-n-morty/static/media/bgImage.b50f5272.jpg"
+  },
+  {
+    "revision": "f1bc3b82ca205cc7ca13be9c96eda804",
+    "url": "/react-ricky-n-morty/static/media/characters-bg.f1bc3b82.jpg"
+  },
+  {
+    "revision": "fedebc05067c804ed0d642081f2c2f5b",
+    "url": "/react-ricky-n-morty/static/media/nebula.fedebc05.jpg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/react-ricky-n-morty/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/react-ricky-n-morty/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/react-ricky-n-morty/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/react-ricky-n-morty/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "d810f491f9327dd064be322c65f045b0",
+    "url": "/react-ricky-n-morty/static/media/pattern.d810f491.svg"
+  },
+  {
+    "revision": "f922029092dba68cab6382c05d794e8e",
+    "url": "/react-ricky-n-morty/static/media/r-n-m-favorites.f9220290.jpg"
+  },
+  {
+    "revision": "c32708bc760e1e6824f6909665539856",
+    "url": "/react-ricky-n-morty/static/media/rick-and-morty-gif.c32708bc.gif"
+  },
+  {
+    "revision": "4a06efb5a2f39feaa2f81b9f3d179a94",
+    "url": "/react-ricky-n-morty/static/media/travolta.4a06efb5.gif"
+  }
+]);
